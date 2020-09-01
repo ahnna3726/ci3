c7de8786b61375072e3e14558d561dc00879d6bb
ahnna <ahnna3726@gmail.com>
2020-09-02 00:33:02 +0900

    Codeigniter First commit
    routes.php
    autoload.php
    config.php
    
    Item, Item_Model
    
    header
    footer
    list
    create
    show
    edit

---
+++ application/views/layout/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Basic Crud operation in Codeigniter 3</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+		  integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+			integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+			crossorigin="anonymous"></script>
+</head>
+<body>
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+	<div class="container">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#">Basic CRUD</a>
+		</div>
+<!--		<div class="collapse navbar-collapse">-->
+<!--			<ul class="nav navbar-nav">-->
+<!--				<li class="active"><a href="#"><span class="glyphicon glyphicon-home"></span> Home</a></li>-->
+<!--				<li><a href="#about"><span class="glyphicon glyphicon-info-sign"></span> About</a></li>-->
+<!--				<li><a href="#contact"><span class="glyphicon glyphicon-envelope"></span> Contact</a></li>-->
+<!--			</ul>-->
+<!--		</div>-->
+	</div>
+</nav>
